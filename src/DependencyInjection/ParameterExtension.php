0a7c8f21afe0ddb4cfb3e527a4d5907d6646beca
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-13 21:17:04 +0100

    ParameterExtension - change Extension class.

--- src/DependencyInjection/ParameterExtension.php
+++ src/DependencyInjection/ParameterExtension.php
@@ -4,9 +4,9 @@ namespace Danilovl\ParameterBundle\DependencyInjection;
 
 use Danilovl\ParameterBundle\Service\ParameterService;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ParameterExtension extends Extension
 {
